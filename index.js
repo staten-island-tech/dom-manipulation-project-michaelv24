0664d999d92d704915a0ff1f73a400d5294a5755
michaelv24 <mvsch20@gmail.com>
2022-11-10 10:15:04 -0500

    DOM Manipulation

---
+++ index.js
@@ -0,0 +1,7 @@
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"),
+  point: document.querySelectorAll(".point"),
+  pointTwo: document.getElementById(".pointTwo"),
+};
+console.log(DOMSelectors.point);
