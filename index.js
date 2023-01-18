75aa223c00945637cc2d381acdd3cb1e65d2a6ea
michaelv24 <mvsch20@gmail.com>
2023-01-17 23:07:51 -0500

    DOM Manipulation Complete

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
-const DOMSelectors = {
-  button: document.getElementById("btn"),
-  box: document.getElementById  ("container-box"),
-  text: document.querySelector("#text"),
-  point: document.querySelectorAll(".point"),
-  pointTwo: document.getElementById(".pointTwo"),
-};
-console.log(DOMSelectors.point);
-
 //The Document Object Model, or DOM, is essentially the elements you write into your HTML. DOMSelectors allow you to easily access your HTML in JS.
 
 // DOMSelectors are objects that hold access to the DOM that you can call upon easily at any point in your code.
-
 const DOMSelectors = {
-  button: document.getElementById("btn"), // getElementById looks for the id that is passed through it, in this case being "btn"
-  text: document.querySelector("#text"), // queryselectors look for elements based on parameters, being '.' or '#'
-  box: document.getElementById("big-black-box"),
-  points: document.querySelectorAll(".point"), // querySelectorAll will access multiple elements with the same class, in this case being "point"
-  null: document.getElementById("point"), // if you try to access a class with getElementById, it will result in "null"
-  empty: document.querySelector(".nothing"),
+  button: document.getElementById("btn"),
+  form: document.getElementById("form"),
+  nameInput: document.getElementById("name"),
+  ratingInput: document.getElementById("rating"),
+  imageInput: document.getElementById("imagelink"),
+  list: document.getElementById("list"),
+  remove: document.getElementsByClassName("remove"),
 };
-const button = document.getElementById("btn");
-console.log(button);
-console.log(DOMSelectors.box);
-console.log(DOMSelectors.button);
-console.log(DOMSelectors.text);
-console.log(DOMSelectors.points);
-console.log(DOMSelectors.points[0]); // this returns the first ever element in the document with class 'point'
-console.log(DOMSelectors.null);
-console.log(DOMSelectors.nothing);\
-// when you look in the console after logging DOMSelectors, you will be able to see the elements that the DOMSelector accesses.
-// if you have a DOMSelector for an element that doesn't exist, it will show up as undefined.
-  DOMSelectors.box.insertAdjacentHTML("beforeend", '<h1>We are an H1</h1>')
+
+function removeDiv(e) {
+  e.parentElement.remove();
+}
+
+function addPoster(name, rating, image) {
+  const box = document.createElement("div");
+  box.insertAdjacentHTML("afterbegin", `<p>Movie Rating: ${rating}</p>`);
+  box.insertAdjacentHTML("afterbegin", `<p>Movie Name: ${name}</p>`);
+  box.insertAdjacentHTML(
+    "beforeend",
+    `<img src=${image} class="movieimg"></img>`
+  );
+  box.className = "movieBox";
+  box.insertAdjacentHTML("beforeend", `<button class="remove">Remove</button>`);
+  DOMSelectors.list.insertAdjacentElement("afterbegin", box);
+}
+
+DOMSelectors.form.addEventListener("submit", function () {
+  addPoster(
+    DOMSelectors.nameInput.value,
+    DOMSelectors.ratingInput.value,
+    DOMSelectors.imageInput.value
+  );
+  DOMSelectors.form.reset();
+  for (let btn of DOMSelectors.remove) {
+    btn.addEventListener("click", function () {
+      this.parentElement.remove();
+    });
+  }
+});
